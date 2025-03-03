2427abb97bf76f77906867b8a9ecd3bc396df317
soo-1212 <depthsoo@naver.com>
2025-03-03 11:52:27 +0900

    new1

---
+++ turtlebot3_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/soo/turtlebot3_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
+. "/home/soo/turtlebot3_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/home/soo/turtlebot3_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
